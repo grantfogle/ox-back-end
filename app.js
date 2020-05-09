2bae251b348ac4d7481a2300b2cda4c433431db3
grantfogle <grantjfogle@gmail.com>
2020-05-08 21:39:29 -0600

    mocked calls for app

--- app.js
+++ app.js
@@ -8,10 +8,19 @@ app.get('/', (req, res) => {
     res.send('it worked duuuuuuude');
 });
 
-
 // get authentication for spotify user
+// app.get('/user-authentication', (req, res) => {});
+// create playlist 
+// app.post('/create-playlist', (req, res) => {});
+// find playlist
+// app.get('/find-playlist', (req, res) => {});
+// search songs
+// app.get('/search-songs', (req, res) => {});
+// add song to playlist
+// app.post('/add-song-to-playlist', (req, res) => {});
+// remove song from playlist
+// app.delete('/remove-song-from-playlist', (req, res) => {});
 
-// 
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
