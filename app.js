17dbc88dd49ffa9e5bded388b6c5fdc11e5b6f4f
grantfogle <grantjfogle@gmail.com>
2020-06-19 21:58:47 -0600

    added additional get and post requests to app.js

--- app.js
+++ app.js
@@ -13,6 +13,34 @@ app.get('/', (req, res) => {
     queries.listAll().then(playlists => res.send(playlists));
 });
 
+// app.get('/find-playlist', (req, res) => {
+//     const { playlistName, password } = req.body;
+//     return queries.getPlaylistByName(playlistName)
+//         .then(playlist => {
+//             // error handling
+//             if (playlist.length === 0) {
+//                 return res.send('Playlist not found, password was incorrect');
+//             }
+//             // handle wrong password
+//             return res.send(playlistId);
+//         })
+// })
+
+// app.post('/create-playlist', (req, res) => {
+//     const { playlistName, password, spotifyId } = req.body;
+//     return queries.getPlaylistByName(playlistName).then(playlist => {
+//         if (playlist.length > 0) {
+//             return res.send('User already exists');
+//         } else {
+//             let newPlaylist = {
+//                 playlistName,
+//                 password: hash,
+//                 spotifyId
+//             }
+//             return queries.createPlaylist(newPlaylist).then(response)
+//         }
+//     })
+// })
 // get playlist by name
 // playlist should have name, id
 // status live?
--- queries.js
+++ queries.js
@@ -2,7 +2,9 @@ const db = require('./database-connection');
 
 module.exports = {
     listAll() {
-        console.log('it ran');
         return db('playlists');
+    },
+    getPlaylistByName(id) {
+        return db('playlists').where('id')
     }
 }
